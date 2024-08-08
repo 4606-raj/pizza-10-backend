cbe894094af60264cffdcbc2cd64a78e99ff441c
Raj Kumar <4606.raj@gmail.com>
2024-08-09 00:10:34 +0530

    validation

--- database/migrations/2024_07_19_065140_create_orders_table.php
+++ database/migrations/2024_07_19_065140_create_orders_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->double('total_amount');
             $table->string('payment_mode');
             $table->json('payment_response')->nullable();
-            $table->enum('order_type', ['delivery', 'pickup', 'dine-in', 'in-car'])->default('delivery');
+            $table->enum('order_type', ['delivery', 'pickup', 'dine_in', 'in_car'])->default('delivery');
             $table->integer('status')->default(1)->comment('1: pending, 2: confirmed');
             $table->timestamps();
         });
