d2f85e5119e9b60174a390e925e94b6f1c234af5
Raj Kumar <4606.raj@gmail.com>
2024-08-28 22:52:17 +0530

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\HomeController;
 
 Route::view('/login', 'auth.login')->name('login');
 
 Route::group(['middleware' => 'admin'], function() {
     
-    Route::get('/', [UserController::class, 'index'])->name('dashboard');
+    Route::get('/', [HomeController::class, 'index'])->name('dashboard');
 
     Route::resource('users', UserController::class);
     Route::resource('orders', OrderController::class);
